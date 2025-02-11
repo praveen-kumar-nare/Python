9803cd9fab3a11c151a6c55a8d358b4f44ab9b91
praveen kumar <narepraveen007@gmail.com>
2025-02-11 18:58:26 +0530

    Add files via upload

---
+++ sorting elements in variable.py
@@ -0,0 +1,17 @@
+class GFG:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def __repr__(self):
+        return str((self.a, self.b))
+
+
+gfg = [GFG("geeks", 1),
+       GFG("computer", 3),
+       GFG("for", 2),
+       GFG("geeks", 4),
+       GFG("science", 3)]
+
+
+print(sorted(gfg, key=lambda x: x.a.lower()))
