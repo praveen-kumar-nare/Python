86926ec9143583e06b853ba59d6016cd7f42664a
praveen kumar <narepraveen007@gmail.com>
2025-02-14 21:23:36 +0530

    Add files via upload

---
+++ empty class.py
@@ -0,0 +1,30 @@
+
+
+
+class Employee: 
+	pass
+
+obj1 = Employee() 
+obj1.name = 'Nikhil'
+obj1.office = 'GeeksforGeeks'
+
+
+obj2 = Employee() 
+obj2.name = 'Abhinav'
+obj2.office = 'GeeksforGeeks'
+obj2.phone = 1234567889
+
+
+
+print("obj1 Details:") 
+print("Name:", obj1.name) 
+print("Office:", obj1.office) 
+print() 
+
+print("obj2 Details:") 
+print("Name:", obj2.name) 
+print("Office:", obj2.office) 
+print("Phone:", obj2.phone) 
+
+
+
