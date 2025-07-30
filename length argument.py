32a2a1a097d7e67dfd926c9090405ddad15b812d
praveen kumar <narepraveen007@gmail.com>
2025-07-30 21:57:29 +0530

    Add files via upload

---
+++ length argument.py
@@ -0,0 +1,10 @@
+ def sum(name,*n): 
+  result =0 
+  for x in n: 
+       result = result + x 
+  print("The Sum by", name, ": ", result) 
+sum(name = 'Robin',10,20,30,40) 
+sum(name = 'Rahul',10,20,30) 
+sum(name = 'Sachin',10,20) 
+sum(name = 'Sourav',10) 
+sum(name = 'Karthi') 
