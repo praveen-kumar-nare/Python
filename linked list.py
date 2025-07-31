12d1df4cc8fd19085291060ad72d22576463762b
praveen kumar <narepraveen007@gmail.com>
2025-07-31 19:09:53 +0530

    Add files via upload

---
+++ linked list.py
@@ -0,0 +1,8 @@
+def insertAtBegin(self, data):
+    new_node = Node(data)
+    if self.head is None:
+        self.head = new_node
+        return
+    else:
+        new_node.next = self.head
+        self.head = new_node
