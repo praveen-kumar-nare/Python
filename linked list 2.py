8d055adbb1ee66dbba2b78cde427d13d6723f60c
praveen kumar <narepraveen007@gmail.com>
2025-08-01 18:13:22 +0530

    Add files via upload

---
+++ linked list 2.py
@@ -0,0 +1,19 @@
+# Method to add a node at any index
+# Indexing starts from 0.
+def insertAtIndex(self, data, index):
+    if (index == 0):
+        self.insertAtBegin(data)
+        return
+
+    position = 0
+    current_node = self.head
+    while (current_node != None and position+1 != index):
+        position = position+1
+        current_node = current_node.next
+
+    if current_node != None:
+        new_node = Node(data)
+        new_node.next = current_node.next
+        current_node.next = new_node
+    else:
+        print("Index not present")
